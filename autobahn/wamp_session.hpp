c4bba62d4ddaa7ff8139d42f0b532861c44bae6f
Nicholas Wiles <nwiles@airware.com>
2015-11-04 18:32:11 -0800

    added define guards to prevent compiler warnings

--- autobahn/wamp_session.hpp
+++ autobahn/wamp_session.hpp
@@ -26,9 +26,18 @@
 #include "wamp_procedure.hpp"
 
 // http://stackoverflow.com/questions/22597948/using-boostfuture-with-then-continuations/
+#ifndef BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE
+#endif
+
+#ifndef BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
 #define BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
+#endif
+
+#ifndef BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
 #define BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
+#endif
+
 #include <boost/asio.hpp>
 #include <boost/thread/future.hpp>
 #include <cstdint>
