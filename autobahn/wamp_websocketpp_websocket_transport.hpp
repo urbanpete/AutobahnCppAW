ef9cd76b12ded67b581451a13e2d97628ba6b2ec
Peter Gissel <pgissel@airware.com>
2016-04-09 16:44:28 -0700

    refactor boost future macros into common header with ifdefs

--- autobahn/wamp_websocketpp_websocket_transport.hpp
+++ autobahn/wamp_websocketpp_websocket_transport.hpp
@@ -31,12 +31,8 @@
 #ifndef AUTOBAHN_WEBSOCKETPP_WEBSOCKET_TRANSPORT_HPP
 #define AUTOBAHN_WEBSOCKETPP_WEBSOCKET_TRANSPORT_HPP
 
-// http://stackoverflow.com/questions/22597948/using-boostfuture-with-then-continuations/
-#ifndef	BOOST_THREAD_PROVIDES_FUTURE
-#define BOOST_THREAD_PROVIDES_FUTURE
-#define BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
-#define BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
-#endif
+#include "boost_config.hpp"
+
 #ifdef _WIN32
 #pragma warning(disable:4996) //Windows XP cancel async IO always fails with operation_not_supported
 //Recommended WebSocket++ settings for Windows
